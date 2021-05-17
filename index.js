1cdc9ff498a866181387269abcad4a8f36eed8fb
Azal Abedi <azal.abedi@gmail.com>
2021-05-17 12:46:43 -0600

    add basic start and stop to timer

--- index.js
+++ index.js
@@ -5,11 +5,24 @@ class Timer {
         this.pauseButton = pauseButton;
 
         this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
     }
 
-    start() {
-        console.log('Time to start the timer!');
-    }
+    // Using an arrow function here to ensure "this" remains as the Timer object
+    start = () => { 
+        this.tick(); 
+            // calling tick() manually before because setInterval takes a second to even start
+        this.interval = setInterval(this.tick, 1000); 
+            // "interval" captures the "ID" of the setInterval so we can clear it in the pause functionality, we use "this" so we can use it in other methods
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        console.log('tick');
+    };
 }
 
 const durationInput = document.querySelector('#duration');
